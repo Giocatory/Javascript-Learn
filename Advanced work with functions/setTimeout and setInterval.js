44b78ac87e1055bc168639635a6515a375a1026f
Giocatory <nivolyas@gmail.com>
2022-01-11 10:30:51 +0300

    work with timeout and interval

--- Advanced work with functions/setTimeout and setInterval.js
+++ Advanced work with functions/setTimeout and setInterval.js
@@ -5,8 +5,10 @@ SetTimeout allows you to call a function once at a certain time interval.
 let timerId = setTimeout(func|code, [delay], [arg1], [arg2], ...)
 
 SetInterVal allows you to call a function regularly, repeating the call at a certain time interval.
+let timerId = setInterval(func|code, [delay], [arg1], [arg2], ...)
 */
 
+// 1
 setTimeout(() => {
     console.log("after delay: 1.5sec");
 }, 1500);
@@ -17,3 +19,19 @@ function timeOutMessage(name, wait) {
 }
 
 setTimeout(timeOutMessage, 2500, "Mike", 1500);
+
+// 2. Display numbers from 1 to 10, every second, after 10 stop the interval
+let count = 0;
+let tenSeconds = setInterval(() => {
+    if (count !== 11) console.log(count++);
+    else clearInterval(tenSeconds);
+}, 1000); // 1...10
+
+// 2.1 Display numbers from 1 to 10, every second, after 10 stop the interval WITH DELAY
+setTimeout(() => {
+    let count = 0;
+    let tenSeconds = setInterval(() => {
+        if (count !== 11) console.log(count++);
+        else clearInterval(tenSeconds);
+    }, 1000); // 1...10
+}, 13000);
