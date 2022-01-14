d89ac8caf4283e000ad6f3977935e26b950bec18
Giocatory <nivolyas@gmail.com>
2022-01-14 17:51:29 +0300

    work with prototypes

--- Prototypes and inheritance/F.prototype.js
+++ Prototypes and inheritance/F.prototype.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let animal = {
+    eats: true,
+};
+
+function Rabbit(name) {
+    this.name = name;
+}
+
+Rabbit.prototype = animal;
+Rabbit.prototype.jump = 'i jump';
+
+let rabbit = new Rabbit("White rabbit"); // rabbit.__proto__ == animal
+
+console.log(rabbit.eats); // true
+console.log(rabbit.jump); // i jump
