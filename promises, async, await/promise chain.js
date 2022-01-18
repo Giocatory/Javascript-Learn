7f963e2a639e122ce8598af3956e45e617ab2c08
Giocatory <nivolyas@gmail.com>
2022-01-18 10:11:10 +0300

    basic knowledge promise, async/await

---
+++ promises, async, await/promise chain.js
@@ -0,0 +1,24 @@
+new Promise(function(resolve, reject) {
+
+    setTimeout(() => resolve(1), 1000); // (*)
+
+}).then(function(result) { // (**)
+
+    console.log(result); // 1
+    return result * 2;
+
+}).then(function(result) { // (***)
+
+    console.log(result); // 2
+    return result * 2;
+
+}).then(function(result) {
+
+    console.log(result); // 4
+    return result * 2;
+
+});
+
+// 1
+// 2
+// 4
